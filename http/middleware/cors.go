0dcfbd8b5a864d8208e37928aea90e6858481d94
wanmin <wanminny@163.com>
2018-10-30 11:36:14 +0800

    cc

--- http/middleware/cors.go
+++ http/middleware/cors.go
@@ -7,7 +7,8 @@ func CorsHeader(next http.Handler) http.Handler{
 	f :=  func(w http.ResponseWriter,req *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
-		w.Header().Set("access-control-allow-credentials","true")
+		w.Header().Set("Access-Control-Allow-Credentials","true")
+
 		next.ServeHTTP(w,req)
 	}
 	return http.HandlerFunc(f)
